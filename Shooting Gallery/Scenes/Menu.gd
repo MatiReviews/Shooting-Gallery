67a8eca292abecebb9cc9ef6269d58bcdf20b690
Matias Szamrynskyj <matiastoo@hotmail.com>
2021-10-19 14:32:06 -0300

    xd

--- Shooting Gallery/Scenes/Menu.gd
+++ Shooting Gallery/Scenes/Menu.gd
@@ -1,16 +1,13 @@
 extends Control
 
-func _ready():	
+func _ready():
+	$CheckButton.pressed = OS.window_fullscreen
+	
 	$Label.text = "Scores\n"
-	
-	#en que iteracion
-#	for score in Globals.scores:
-#		$Label.text += str(score) + "\n"
-	
 	for i in Globals.scores.size():
-		$Label.text += str(i+1) + ": " + str(Globals.scores[i]) + "\n"
-#		$Label.text += "Run %S\n" % [i+1, Globals.score[i]]
-	pass
+		$Label.text += "Run %s: %s\n" % [i+1, Globals.scores[i]]
+	
+
 
 func _on_Play_pressed():
 	var err = get_tree().change_scene("res://Scenes/Level.tscn")
@@ -22,11 +19,15 @@ func _on_Quit_pressed():
 	get_tree().quit()
 	pass # Replace with function body.
 
-
 func _on_CheckButton_pressed():
 	pass # Replace with function body.
 
 
 func _on_CheckButton_toggled(button_pressed):
 	Globals.set_fullscreen(button_pressed)
+	pass # Replace with function body.
+
+
+func _on_HSlider_value_changed(value):
+	Globals._set_Master_Volume(value)
 	pass # Replace with function body.
--- Shooting Gallery/Singletons/Globals.gd
+++ Shooting Gallery/Singletons/Globals.gd
@@ -1,27 +1,47 @@
 extends Node
 
 var score : = 0
-#
+
 var scores = []
-#
+
 const config_path = "user://settings.cfg"
 var config
 
+var xd :float = 0.0
+
 func _ready():
-	var config = ConfigFile.new()
+	config = ConfigFile.new()
 	if config.load(config_path) != OK:
 		print("Error loading config")
 		config.set_value("graphics", "fullscreen", false)
-		#config.set_value("stats", "scores", scores)
+		config.set_value("stats", "scores", scores)
 		config.save(config_path)
 	else:
 		print("Config Loaded")
-		set_fullscreen(config.get_value("graphics","fullscreen", false))
+		set_fullscreen(config.get_value("graphics","fullscreen", []))
+		scores = config.get_value("stats", "scores", [])
+		xd = config.get_value("Audio", "Master", 100.0)
+		_set_Master_Volume(80)
 
 func _add_Scores(new_score: int):
 	scores.append(new_score)
+	config.set_value("stats", "scores", scores)
+	config.save(config_path)
+
+func _get_Score_List() -> Array:
+	return scores
 
 func set_fullscreen(enabled: bool):
 	OS.window_fullscreen = enabled
-#	config.set_value("graphics","fullscreen", enabled)
-#	config.save(config_path)
+	config.set_value("graphics","fullscreen", enabled)
+	config.save(config_path)
+
+func _set_Master_Volume(volume : float):
+	AudioServer.set_bus_volume_db(0, linear2db(volume/100))
+	config.set_value("Audio", "Master", volume)
+	config.save(config_path)
+	
+func _set_Sfx_Volume(volume : float):
+	AudioServer.set_bus_volume_db(2, linear2db(volume/100))
+	config.set_value("Audio", "Sfx", volume)
+	config.save(config_path)
