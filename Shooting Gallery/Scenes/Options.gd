4253ef08cfd008b4e2b906ad667a797fa22b4e72
Matias Szamrynskyj <matiastoo@hotmail.com>
2021-10-26 21:27:02 -0300

    Erased blank space

--- Shooting Gallery/Scenes/Options.gd
+++ Shooting Gallery/Scenes/Options.gd
@@ -9,7 +9,6 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$CheckButton.pressed = OS.window_fullscreen
-	
 	pass # Replace with function body.
 
 
